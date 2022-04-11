73be215d4be60d481231547909df44313e4efa49
Code Sandbox <hello@codesandbox.io>
2022-04-11 11:46:10 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b7395079db356b6c12c3efb98861df16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "46bde2a7ec2edd55c68d",
+    "url": "https://yoottana-prapbuntarik.github.io/csb-1hdsxg/static/css/main.ca441a1e.chunk.css"
+  },
+  {
+    "revision": "46bde2a7ec2edd55c68d",
+    "url": "https://yoottana-prapbuntarik.github.io/csb-1hdsxg/static/js/main.46bde2a7.chunk.js"
+  },
+  {
+    "revision": "6a52136217d5b117be2b",
+    "url": "https://yoottana-prapbuntarik.github.io/csb-1hdsxg/static/css/1.bd88aaed.chunk.css"
+  },
+  {
+    "revision": "6a52136217d5b117be2b",
+    "url": "https://yoottana-prapbuntarik.github.io/csb-1hdsxg/static/js/1.6a521362.chunk.js"
+  },
+  {
+    "revision": "d9f99adc7754746eac73",
+    "url": "https://yoottana-prapbuntarik.github.io/csb-1hdsxg/static/js/runtime~main.d9f99adc.js"
+  },
+  {
+    "revision": "1849d83bdeb0582ea84449e3bb9fcc27",
+    "url": "https://yoottana-prapbuntarik.github.io/csb-1hdsxg/index.html"
+  }
+];
